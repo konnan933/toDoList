bb4d9a1e8077450ccca50e4563f532270fc8679f
KncskBenedek <koncsikbenedek@gmail.com>
2022-08-12 21:30:14 +0200

    v0.3

--- cont/regisztracio.php
+++ cont/regisztracio.php
@@ -1,10 +1,8 @@
 <?php
-    print_r($_POST);
-    include_once 'Csatlakozas';
+    var_dump($_POST);
+   /*  include_once 'Csatlakozas';
     $ab = new Csatlakozas();
     $sikerE = $ab->regisztral($_POST['reg_email'], $_POST['reg_szoveg'], $_POST['reg_jelszo']);
 
-    echo $sikerE;
-
-
+    echo $sikerE; */
 ?>
