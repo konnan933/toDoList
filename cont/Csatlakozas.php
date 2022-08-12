5d3a76b36f358816ed9c2a7dabf4323887b4e92e
KncskBenedek <koncsikbenedek@gmail.com>
2022-08-12 21:57:27 +0200

    működő regisztráció felvitel

--- cont/Csatlakozas.php
+++ cont/Csatlakozas.php
@@ -9,14 +9,18 @@ class Csatlakozas
 
     public function __construct()
     {
+        
         $this->kapcsolat =  new mysqli($this->host,$this->felhasznaloNev, $this->jelszo, $this->abNev);
 
     }
 
     public function regisztral($email,$felh,$jelszo){
-        $sqlInsert = "INSERT INTO `felhasznalo` (`id`, `felhNev`, `jel`, `email`) VALUES (NULL, '$felh', ' MD5('$jelszo')', '$email' );";
-        ;
+        
+       
+        $sqlInsert = "INSERT INTO `felhasznalo`(`id`, `felhNev`, `jel`, `email`) VALUES (NULL, '$felh',  MD5('$jelszo'), '$email' )";
+        
         $eredmeny = $this->kapcsolat->query($sqlInsert);
+        
         return $eredmeny;
     }
     public function belepes($email, $jelszo){
--- cont/regisztracio.php
+++ cont/regisztracio.php
@@ -1,8 +1,12 @@
 <?php
-    var_dump($_POST);
-   /*  include_once 'Csatlakozas';
+    //var_dump($_POST);
+    include_once 'Csatlakozas.php';
     $ab = new Csatlakozas();
     $sikerE = $ab->regisztral($_POST['reg_email'], $_POST['reg_szoveg'], $_POST['reg_jelszo']);
-
-    echo $sikerE; */
+    /* echo $sikerE; */
+    if($sikerE){
+        echo "siker";
+    }else{
+        echo "nem siker";
+    }
 ?>
