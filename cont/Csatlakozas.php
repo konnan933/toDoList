937ee3d0d15bc7532b02ab5af5b68745fd2aabf0
KncskBenedek <koncsikbenedek@gmail.com>
2022-08-16 22:38:23 +0200

    működő belépés

--- cont/Csatlakozas.php
+++ cont/Csatlakozas.php
@@ -17,19 +17,23 @@ class Csatlakozas
     public function regisztral($email,$felh,$jelszo){
         
        
-        $sqlInsert = "INSERT INTO `felhasznalo`(`id`, `felhNev`, `jel`, `email`) VALUES (NULL, '$felh',  MD5('$jelszo'), '$email' )";
+        $sqlInsert = "INSERT INTO `felhasznalo`(`id`, `felhNev`, `jel`, `email`) VALUES (NULL, '$felh',  md5('$jelszo'), '$email' )";
         
         $eredmeny = $this->kapcsolat->query($sqlInsert);
         
         return $eredmeny;
     }
     public function belepes($email, $jelszo){
-        $sql = 'Select  as belep from felhasznalo where "'.$email.'" = email';
+        $md5Jel = md5($jelszo);
+        $sql = "Select if( '$md5Jel'= f.jel , true, false) as belep from felhasznalo as f where  '$email' = f.email";
         $valasz = $this->kapcsolat->query($sql);
         $sikeresBelep = false;
         if($valasz->num_rows == 1){
+            
             while($sor = $valasz->fetch_assoc()){
-                $sikeresBelep =  intval($sor["belep"]) == 1 ? true:false;
+                
+                $sikeresBelep =  intval($sor["belep"]) == 1;
+                //var_dump($sikeresBelep);
             }
         }
         return $sikeresBelep;
--- cont/login.php
+++ cont/login.php
@@ -2,10 +2,13 @@
 include_once "Csatlakozas.php";
 $abKapcsolat = new Csatlakozas();
 $belepes = $abKapcsolat->belepes($_POST['bej_email'],$_POST['bej_jelszo']);
+
 if($belepes){
     header("Location: ../view/to_do.html");
+   
 }
 else{
-    
+    header("Location: ../view/index.html");
+   
 }
 ?>
